aca9b730626fec595a58f3bd51ea01e67c2665d7
Binod Swain <binodkumarswain94@gmail.com>
2020-04-25 02:39:05 +0530

    fix(): added focus management for animate: false
    updated JSDocs for all functions
    created sepate shift methods

--- src/backDrop.js
+++ src/backDrop.js
@@ -5,22 +5,23 @@ import { STATES } from "./index";
 
 const backdrop = (props) => {
     const { overlayState, clickDismiss = true, closeOverlay } = props;
+
     const inTransition = overlayState === STATES.OPENING || overlayState === STATES.CLOSING;
+    /**
+     * Closes overlay when the backdrop is clicked and not
+     * in transition mode (opening or closing)
+     * @param {Event} e
+     */
+    const handleClick = (e) => {
+        const isBackdrop = e.target.dataset && e.target.dataset.comp === "backdrop";
+        const canClose = clickDismiss && !inTransition;
+        if (canClose && isBackdrop) {
+            closeOverlay();
+        }
+    };
 
     return (
-        <div
-            className={styles["backdrop"]}
-            data-comp="backdrop"
-            onClick={
-                clickDismiss && !inTransition
-                    ? (e) => {
-                        if (e.target.dataset && e.target.dataset.comp === "backdrop") {
-                            closeOverlay();
-                        }
-                    }
-                    : undefined
-            }
-        >
+        <div className={styles["backdrop"]} data-comp="backdrop" onClick={handleClick}>
             {props.children}
         </div>
     );
